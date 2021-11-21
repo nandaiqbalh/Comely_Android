525172e1ada9ab255c2547b06b492b79b2a6973d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-11-22 00:05:28 +0700

    Bottom Nav View udah berfungsi

--- app/src/main/java/com/nandaiqbalh/comely/MainActivity.java
+++ app/src/main/java/com/nandaiqbalh/comely/MainActivity.java
@@ -1,12 +1,15 @@
 package com.nandaiqbalh.comely;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-import android.view.WindowManager;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.nandaiqbalh.comely.fragment.AkunFragment;
 import com.nandaiqbalh.comely.fragment.HomeFragment;
 import com.nandaiqbalh.comely.fragment.KeranjangFragment;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager = getSupportFragmentManager();
     Fragment active = homeFragment;
 
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +33,38 @@ public class MainActivity extends AppCompatActivity {
         // full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // menambahkan fragment ke dalam frame layout lewat fragment manager
-        fragmentManager.beginTransaction().add(R.id.container, homeFragment).show(homeFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.container, keranjangFragment).hide(keranjangFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.container, akunFragment).hide(akunFragment).commit();
+        // default fragment
+        Fragment homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        // bottom navigation view
+        bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_beranda:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_keranjang:
+                            selectedFragment = new KeranjangFragment();
+                            break;
+                        case R.id.nav_akun:
+                            selectedFragment = new AkunFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
 }
