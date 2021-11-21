cc3b49e74c7c476e126489461703ba6144a4a319
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-11-21 19:01:30 +0700

    Menambahkan Fragment ke MainActivity lewat FragmentManager

--- app/src/main/java/com/nandaiqbalh/comely/MainActivity.java
+++ app/src/main/java/com/nandaiqbalh/comely/MainActivity.java
@@ -1,11 +1,24 @@
 package com.nandaiqbalh.comely;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.nandaiqbalh.comely.fragment.AkunFragment;
+import com.nandaiqbalh.comely.fragment.HomeFragment;
+import com.nandaiqbalh.comely.fragment.KeranjangFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    // FRAGMENT
+    HomeFragment homeFragment = new HomeFragment();
+    KeranjangFragment keranjangFragment = new KeranjangFragment();
+    AkunFragment akunFragment = new AkunFragment();
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    Fragment active = homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         // full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // menambahkan fragment ke dalam frame layout lewat fragment manager
+        fragmentManager.beginTransaction().add(R.id.container, homeFragment).show(homeFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.container, keranjangFragment).show(keranjangFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.container, akunFragment).show(akunFragment).commit();
 
     }
 }
