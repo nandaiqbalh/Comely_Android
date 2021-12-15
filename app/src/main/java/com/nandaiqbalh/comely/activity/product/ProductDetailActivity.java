90265f3c501aa3cd0fa1745c057e1ba993dd300f
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-12-15 11:15:54 +0700

    Layouting ProductDetail

---
+++ app/src/main/java/com/nandaiqbalh/comely/activity/product/ProductDetailActivity.java
@@ -0,0 +1,16 @@
+package com.nandaiqbalh.comely.activity.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nandaiqbalh.comely.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
