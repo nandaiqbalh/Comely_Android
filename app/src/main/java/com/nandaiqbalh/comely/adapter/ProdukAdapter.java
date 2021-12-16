f2dd33dff69f3e9935a30a385b8b3c23fcf40b44
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-12-16 23:43:49 +0700

    Show discount with discount amount

--- app/src/main/java/com/nandaiqbalh/comely/adapter/ProdukAdapter.java
+++ app/src/main/java/com/nandaiqbalh/comely/adapter/ProdukAdapter.java
@@ -2,12 +2,14 @@ package com.nandaiqbalh.comely.adapter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.LongDef;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,7 +17,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.gson.Gson;
 import com.nandaiqbalh.comely.R;
 import com.nandaiqbalh.comely.activity.product.ProductDetailActivity;
-import com.nandaiqbalh.comely.model.brand.Brand;
 import com.nandaiqbalh.comely.model.produk.Produk;
 import com.squareup.picasso.Picasso;
 
@@ -33,7 +34,7 @@ public class ProdukAdapter extends RecyclerView.Adapter<ProdukAdapter.myViewHold
     Produk produk;
 
     // constructor
-    public ProdukAdapter(Activity activity, ArrayList<Produk> dataHolder){
+    public ProdukAdapter(Activity activity, ArrayList<Produk> dataHolder) {
         this.activity = activity;
         this.dataHolder = dataHolder;
     }
@@ -50,6 +51,33 @@ public class ProdukAdapter extends RecyclerView.Adapter<ProdukAdapter.myViewHold
     public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
         holder.namaProduk.setText(dataHolder.get(position).getProduct_name_eng());
         holder.hargaProduk.setText(NumberFormat.getCurrencyInstance(new Locale("in", "ID")).format(Integer.valueOf(dataHolder.get(position).getSelling_prize())));
+
+        if (dataHolder.get(position).getDiscount_prize().equalsIgnoreCase("0")) {
+            holder.diskonProduk.setVisibility(View.GONE);
+        } else {
+            double diskonPrize, sellingPrize, diskonFinal;
+
+            try {
+                diskonPrize = Double.parseDouble(dataHolder.get(position).getDiscount_prize());
+            } catch (NumberFormatException e) {
+                diskonPrize = 0;
+            }
+
+            try {
+                sellingPrize = Double.parseDouble(dataHolder.get(position).getSelling_prize());
+            } catch (NumberFormatException e) {
+                sellingPrize = 0;
+            }
+
+
+            diskonFinal = diskonPrize / sellingPrize * 100;
+            int angkaSignifikan = 1;
+            double tempDiskon = Math.pow(10, angkaSignifikan);
+            double diskonTampil = (double) Math.round(diskonFinal*tempDiskon)/tempDiskon;
+
+            holder.diskonProduk.setText(diskonTampil + "%");
+        }
+
         // Gambar
         String imageURL = "http://192.168.160.130/udemy/latihan_ecommerce/public/" + dataHolder.get(position).getProduct_thumbnail();
         Picasso.get()
@@ -77,10 +105,11 @@ public class ProdukAdapter extends RecyclerView.Adapter<ProdukAdapter.myViewHold
         return dataHolder.size();
     }
 
-    class myViewHolder extends RecyclerView.ViewHolder{
+    class myViewHolder extends RecyclerView.ViewHolder {
 
         ImageView gambarProduk;
         TextView namaProduk, hargaProduk;
+        TextView diskonProduk;
         CardView layoutProduct;
 
         public myViewHolder(@NonNull View itemView) {
@@ -88,6 +117,7 @@ public class ProdukAdapter extends RecyclerView.Adapter<ProdukAdapter.myViewHold
             gambarProduk = itemView.findViewById(R.id.img_gambar_produk);
             namaProduk = itemView.findViewById(R.id.tv_nama_produk);
             hargaProduk = itemView.findViewById(R.id.tv_harga_produk);
+            diskonProduk = itemView.findViewById(R.id.tv_diskon_produk);
 
             // layout card view
             layoutProduct = itemView.findViewById(R.id.cv_layout_product);
